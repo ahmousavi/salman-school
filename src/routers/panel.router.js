5f05a57a6fea80389082061a1e57d569617d2a6a
Amir Mousavi <moosavisabet@gmail.com>
2019-06-16 15:10:42 +0430

    letters

--- src/routers/panel.router.js
+++ src/routers/panel.router.js
@@ -3,26 +3,37 @@ const User = require('../models/user.model')
 const Employee = require('../models/employee.model')
 const { checkToken } = require('../utiles')
 const Letter = require('../models/letter.model')
-router.get('/', checkToken ,function (req, res) {
+router.get('/', checkToken, function (req, res) {
     console.log('CheckToken', req.user);
-    
+
     if (req.user.user_type === 'Student') {
-        res.render('panel_stu', {user: res.user})
+        res.render('panel_stu', { user: res.user })
     }
     else if (req.user.user_type === 'Employee') {
-        res.render('panel_emp', {user: res.user})
+        res.render('panel_emp', { user: res.user })
     }
 })
 
-
 router.get('/letter', checkToken, function (req, res) {
+    Letter.find({ reciver: req.user._id }).populate('sender')
+        .then(recivedLetters => {
+            Letter.find({ reciver: req.user._id }).populate('reciver')
+                .then(sentLetters => {
+                    res.render('letterBox', {sentLetters, recivedLetters})
+                })
+                .catch(err => res.render('error', { error: err }))
+        })
+        .catch(err => res.render('error', { error: err }))
+})
+
+router.get('/letter/new', checkToken, function (req, res) {
     Employee.find({}, (err, emps) => {
         if (err) {
-            res.render('error', {error: err})
+            res.render('error', { error: err })
         }
         else {
-            let a = [1,2,3,4,5]
-            res.render('sendLetter', {user: req.user, employees: emps});
+            let a = [1, 2, 3, 4, 5]
+            res.render('sendLetter', { user: req.user, employees: emps });
         }
     })
 })
@@ -34,9 +45,9 @@ router.post('/letter', checkToken, function (req, res) {
     letter.title = req.body.title
     letter.text = req.body.text
     letter.save().then(letter => {
-        res.send({...letter})
+        res.send({ ...letter })
     })
-    .catch(err => res.render('error', {error: err}))
+        .catch(err => res.render('error', { error: err }))
 })
 
 
